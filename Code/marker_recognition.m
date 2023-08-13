e68c5b64b2cb5c5fdbc5bf812442e778ceb2ad2d
gabrielle <gabrielle@10.80.17.110>
2023-08-13 18:28:14 +0200

    Added comments

--- Code/marker_recognition.m
+++ Code/marker_recognition.m
@@ -1,9 +1,7 @@
+%Read test marker
 img = imread("7x7_test.png");
-% test = [1 2 3 4];
-% command = "test.py -i" + img;
-% command = string(command);
-res = pyrunfile("test.py", "disp", img=img);
-res = double(py.array.array('d',py.numpy.nditer(res)));
-res = reshape(res,[1138 1156 3]);
-imshow(res)
+% call detect_marker.py to get the marker id
+id = pyrunfile("detect_marker.py", "ids", img=img);
+disp(id)
+
 
