d76859346718b4ba7fcb5c8e0b6ef20f1a357fb9
gabrielle <gabrielle@10.80.17.110>
2023-08-13 19:37:32 +0200

    started to implement testing, improved recognition

--- Code/marker_recognition.m
+++ Code/marker_recognition.m
@@ -1,7 +1,19 @@
-%Read test marker
-img = imread("7x7_test.png");
-% call detect_marker.py to get the marker id
-id = pyrunfile("detect_marker.py", "ids", img=img);
-disp(id)
+classdef marker_recognition
+    methods
+        function [id, found]=find(obj, img)
+            % call detect_marker.py to get the marker id
+            id = pyrunfile("detect_marker.py", "ids", img=img);
+            
+            % replace the py.none ids with 0 and record where they occured
+            found = isa(id, "py.numpy.ndarray");
+            if not(found)
+                id = 0;
+            end
+        end
+    end
+end
 
-
+%Current marker settings: 
+%size: 70mm
+%dict: cv.aruco.DICT_7X7_100
+%generated using: https://chev.me/arucogen/
