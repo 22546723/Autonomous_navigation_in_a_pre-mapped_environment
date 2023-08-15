ef7ea450629a6dd1aabb22a9a4915bdbb5154345
gabrielle <gabrielle@localhost>
2023-08-15 10:15:30 +0200

    marker recognition works again

--- Code/marker_recognition.m
+++ Code/marker_recognition.m
@@ -3,7 +3,7 @@ classdef marker_recognition
         function [id, found]=find(obj, img)
             % call detect_marker.py to get the marker id
             id = pyrunfile("detect_marker.py", "ids", img=img);
-            
+
             % replace the py.none ids with 0 and record where they occured
             found = isa(id, "py.numpy.ndarray");
             if ~found
