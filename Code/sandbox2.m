b4f2ecc39b8b4dddf157f73afe517f5f2f43a9a1
22546723 <22546723@firga-m2003-01>
2023-10-02 16:54:30 +0200

    no changes

--- Code/sandbox2.m
+++ Code/sandbox2.m
@@ -1,10 +1,33 @@
-map =  load('/home/22546723/Documents/MATLAB/Skripsie/Code/maps/weighted_graph_test/map.mat');
-map = map.map_data;
+x = [0 0 2];
+y = [0 2 3];
+R_min = 0.1; %m
 
-planner = route_planner(map);
-[path, distance, edgepath]  = plot_route(planner, 1, 3);
-ref_signal = convert_to_ref(planner, path);
+m_ab = (y(2) - y(1))/(x(2) - x(1));
+m_bc = (y(3) - y(2))/(x(3) - x(2));
 
-G = map.weighted_graph;
-p = plot(G, 'EdgeLabel', G.Edges.Weight);
-highlight(p, 'Edges', edgepath)
+c_ab = y(2) - m_ab*x(2);
+c_bc = y(2) - m_bc*x(2);
+
+diff_x_ab = R_min/sqrt(1+m_ab^2);
+pnt_ab = [diff_x_ab + x(2); m_ab*diff_x_ab + y(2)];
+
+diff_x_bc = R_min/sqrt(1+m_bc^2);
+pnt_bc = [diff_x_bc + x(2); m_bc*diff_x_bc + y(2)];
+
+m_ab_p = -1/m_ab;
+m_bc_p = -1/m_bc;
+
+c_ab_p = pnt_ab(2) - m_ab_p*pnt_ab(1);
+c_bc_p = pnt_bc(2) - m_bc_p*pnt_bc(1);
+
+x_mid = (c_bc_p - c_ab_p)/(m_ab_p - m_bc_p);
+y_mid = m_ap_p * x_mid + c_ab_p;
+
+R = sqrt((x_mid - pnt_ab(1))^2 + (y_mid - pnt_ab(2))^2);
+
+hold on
+plot(x, y)
+plot(pnt_ab(1), pnt_ab(2), pnt_bc(1), pnt_bc(2), 'o')
+plot(x_mid, y_mid, 'o')
+hold off
+grid on
