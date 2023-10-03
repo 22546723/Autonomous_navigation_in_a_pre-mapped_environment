0c6e908c9113798cf50fa9291486787a6db96065
gabrielle <gabrielle@localhost>
2023-10-03 20:00:03 +0200

    meh

--- Code/sandbox2.m
+++ Code/sandbox2.m
@@ -1,1 +1,16 @@
+% pnt = [2; 1];
+% R = 1;
+% x = pnt(1)-R:0.2:pnt(1)+R;
+% 
+% dx = x-pnt(1);
+% theta = acos(dx/R);
+% y = R*sin(theta); 
+% yp = pnt(2) + abs(y);
+% yn = pnt(2) - abs(y);
+% 
+% plot(x, yp, x, yn)
+% grid on
 
+theta = 0:(2*pi/10):2*pi;
+pnt1 = [-0.0316; 0.0949];
+pnt2 = []
