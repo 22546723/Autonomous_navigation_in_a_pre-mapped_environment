659c47da5b043e59f0e0ff345830b30d27c62c8b
22546723 <22546723@sun.ac.za>
2023-10-25 09:15:53 +0200

    car follows ref angular velocity

--- Code/sandbox2.m
+++ Code/sandbox2.m
@@ -1,2 +1,5 @@
-speed = get(data, 1).Values.Data;
-torque = get(data, 2).Values.Data;
+% speed = get(data, 1).Values.Data;
+% torque = get(data, 2).Values.Data;
+s = tf('s');
+G_s = 1/s;
+sisotool('rlocus', G_s)
