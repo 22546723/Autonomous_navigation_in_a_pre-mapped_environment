0bb86bbdb60f921429e732ba8006d04299d0dde4
22546723 <22546723@sun.ac.za>
2023-10-26 18:47:03 +0200

    used zeiger-nichols for tuning, seems to work for the transfer function

--- Code/sandbox2.m
+++ Code/sandbox2.m
@@ -1,15 +1,6 @@
-syms s
-ts = 0.148;
-
-zeta = 1;
-sigma = 4/ts
-wn = sigma/zeta
-wd = wn* sqrt(1-zeta^2)
-
-poles = [(-sigma + 1i*wd); (-sigma - 1i*wd)]
-cl = (s - poles(1))*(s - poles(2))
-
-temp = [1 (-poles(1) - poles(2)) (poles(1)*poles(2))]
-
+s = -40 + 1i*27;
+G = 27.03^2/(s*(s+27.03)^2);
+D = (s-211.7)/s;
+K = 1/abs(G)
 
 
