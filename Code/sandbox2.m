b54ae86e9c47af57d94f87af98956a6f3f6fd343
22546723 <22546723@sun.ac.za>
2023-10-26 14:48:04 +0200

    torque control done

--- Code/sandbox2.m
+++ Code/sandbox2.m
@@ -1,29 +1,15 @@
-torque = out.simout.torque.Data;
-w_L = out.simout.w_L_act.Data;
-w_R = out.simout.w_R_act.Data;
-v_L = out.simout.v_L.Data;
-v_R = out.simout.v_R.Data;
+syms s
+ts = 0.148;
+
+zeta = 1;
+sigma = 4/ts
+wn = sigma/zeta
+wd = wn* sqrt(1-zeta^2)
+
+poles = [(-sigma + 1i*wd); (-sigma - 1i*wd)]
+cl = (s - poles(1))*(s - poles(2))
+
+temp = [1 (-poles(1) - poles(2)) (poles(1)*poles(2))]
 
 
-hold on
-yyaxis left
-plot(torque(1:length(w_L)), w_L)
-plot(torque(1:length(w_R)), w_R)
 
-yyaxis right
-plot(torque(1:length(v_L)), v_L)
-plot(torque(1:length(v_R)), v_R)
-
-hold off
-grid on
-
-yyaxis left
-ylabel("Wheel speed [rad/s]")
-legend("Left wheel", "Right wheel")
-
-yyaxis right
-ylabel("Wheel speed [m/s]")
-legend("Left wheel", "Right wheel")
-
-xlabel("Torque [Nm]")
-title("Torque vs wheel speed")
