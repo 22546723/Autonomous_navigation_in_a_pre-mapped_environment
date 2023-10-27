8d5ef1c6f44df0d712a203fccedd18ad40727336
22546723 <22546723@sun.ac.za>
2023-10-27 15:43:06 +0200

    no real changes

--- Code/sandbox2.m
+++ Code/sandbox2.m
@@ -1,6 +1,16 @@
-s = -40 + 1i*27;
-G = 27.03^2/(s*(s+27.03)^2);
-D = (s-211.7)/s;
-K = 1/abs(G)
+ts = 0.1;
+zeta = 0.9;
+
+sigma = 4/ts
+
+wn = sigma/zeta
+
+wd = wn*sqrt(1-zeta^2)
 
 
+s = -40 + 1i*19.37;
+
+den_G = den(1)*s^2 + den(2)*s + den(3);
+G = 100/s
+
+Kd = 1/abs(G)
