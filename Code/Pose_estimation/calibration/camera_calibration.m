1a3fe5573f8c695ac35b35d48e495b0d2c3f6e2e
22546723 <22546723@sun.ac.za>
2023-10-29 14:52:22 +0200

    camera calibration done

--- Code/Pose_estimation/calibration/camera_calibration.m
+++ Code/Pose_estimation/calibration/camera_calibration.m
@@ -1,32 +1,33 @@
-% Determines the camera intrinsics
-
-downloadURL  = "https://github.com/AprilRobotics/apriltag-imgs/archive/master.zip";
-dataFolder   = fullfile(tempdir,"apriltag-imgs",filesep); 
-options      = weboptions('Timeout', Inf);
-zipFileName  = fullfile(dataFolder,"apriltag-imgs-master.zip");
-folderExists = exist(dataFolder,"dir");
-
-% Create a folder in a temporary directory to save the downloaded file.
-if ~folderExists  
-    mkdir(dataFolder); 
-    disp("Downloading apriltag-imgs-master.zip (60.1 MB)...") 
-    websave(zipFileName,downloadURL,options); 
-    
-    % Extract contents of the downloaded file.
-    disp("Extracting apriltag-imgs-master.zip...") 
-    unzip(zipFileName,dataFolder); 
-end
+% % Determines the camera intrinsics
+% 
+% downloadURL  = "https://github.com/AprilRobotics/apriltag-imgs/archive/master.zip";
+% dataFolder   = fullfile(tempdir,"apriltag-imgs",filesep); 
+% options      = weboptions('Timeout', Inf);
+% zipFileName  = fullfile(dataFolder,"apriltag-imgs-master.zip");
+% folderExists = exist(dataFolder,"dir");
+% 
+% % Create a folder in a temporary directory to save the downloaded file.
+% if ~folderExists  
+%     mkdir(dataFolder); 
+%     disp("Downloading apriltag-imgs-master.zip (60.1 MB)...") 
+%     websave(zipFileName,downloadURL,options); 
+%     
+%     % Extract contents of the downloaded file.
+%     disp("Extracting apriltag-imgs-master.zip...") 
+%     unzip(zipFileName,dataFolder); 
+% end
 
 % Set the properties of the calibration pattern.
 tagArrangement = [5,8];
 tagFamily = "tag36h11";
-
-% Generate the calibration pattern using AprilTags.
-tagImageFolder = fullfile(dataFolder,"apriltag-imgs-master",tagFamily);
-imdsTags = imageDatastore(tagImageFolder);
-calibPattern = helperGenerateAprilTagPattern(imdsTags,tagArrangement,tagFamily);
+% 
+% % Generate the calibration pattern using AprilTags.
+% tagImageFolder = fullfile(dataFolder,"apriltag-imgs-master",tagFamily);
+% imdsTags = imageDatastore(tagImageFolder);
+% calibPattern = helperGenerateAprilTagPattern(imdsTags,tagArrangement,tagFamily);
 
 % Read and localize the tags in the calibration pattern.
+calibPattern = imread("april_board.png");
 [tagIds, tagLocs] = readAprilTag(calibPattern,tagFamily);
 
 % Sort the tags based on their ID values.
@@ -38,11 +39,12 @@ tagLocs = reshape(permute(tagLocs,[1,3,2]),[],2);
 
 % Convert the AprilTag corner locations to checkerboard corner locations.
 checkerIdx = helperAprilTagToCheckerLocations(tagArrangement);
-imagePoints = tagLocs(checkerIdx(:),:);
+imagePoints = tagLocs(checkerIdx(:, 1:5),:);
 
 
 % Create an imageDatastore object to store the captured images.
-imdsCalib = imageDatastore("calib_images/");
+imdsCalib = imageDatastore("frames/");
+%imgCalib = imread("Screenshot from calib.avi - 3.png");
 
 % Detect the calibration pattern from the images.
 [imagePoints,boardSize] = helperDetectAprilTagCorners(imdsCalib,tagArrangement,tagFamily);
@@ -52,8 +54,8 @@ tagSize = 16; % in millimeters
 worldPoints = generateCheckerboardPoints(boardSize, tagSize);
 
 % Determine the size of the images.
-I = readimage(imdsCalib,1);
-imageSize = size(I,1:2);
+%I = readimage(imdsCalib,1);
+imageSize = size(imdsCalib,1:2);
 
 % Estimate the camera parameters.
 params = estimateCameraParameters(imagePoints,worldPoints,ImageSize=imageSize);
