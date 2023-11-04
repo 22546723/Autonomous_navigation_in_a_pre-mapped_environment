87ca35b074d1f42716595f691b88b3f110914eab
gabrielle <gabrielle@localhost>
2023-11-04 14:31:16 +0200

    removed a bunch of old stuff

--- Code/Old stuff/assemble_map.m
+++
@@ -1,3 +0,0 @@
-function map_data = assemble_map(node_data, graph_data, marker_data)
-map_data = map(node_data, graph_data, marker_data);
-end
--- Code/Old stuff/init_full_system_workspace.m
+++
@@ -1,5 +0,0 @@
-file_name = "Maps/straight_line_test_setup";
-load(file_name);
-
-cam_intrinsics = load('calibration/camera_params.mat');
-cam_intrinsics = cam_intrinsics.params.Intrinsics;
